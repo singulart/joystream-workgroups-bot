2b61deda9873050143f9fe2cb5b68ea80780b7cd
singulart <serpian@gmail.com>
2022-01-22 23:10:29 +0200

    initial

---
+++ src/main/java/net/joystream/community/tools/discord/workgroupsbot/WorkGroupsBotApplication.java
@@ -0,0 +1,13 @@
+package net.joystream.community.tools.discord.workgroupsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkGroupsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkGroupsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/joystream/community/tools/discord/workgroupsbot/WorkGroupsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package net.joystream.community.tools.discord.workgroupsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkGroupsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
